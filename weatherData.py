879f350633bff06b5bd06d7ee0a3825557d7c406
abc <abc@abcs-MacBook-Pro.local>
2015-04-29 06:56:51 +0530

    Figured out how to fit and make the model

--- weatherData.py
+++ weatherData.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import six
 import main as da
-
+import  sys
 
 #Notes : more then 2 inch of snow or more then 1 inch of rain
 
@@ -15,7 +15,7 @@ data.next() # this removes the header in the file i.e the first collumn
 data = [[i for i in y] for y in data]
 
 
-# working with weather_________________________________________________________
+# working with weather______________________________________________________
 key = csv.reader(open(r'key.csv')) #load the station data and the store place
 key.next()
 key = [[int(i) for i in y ]for y in key]
@@ -26,18 +26,24 @@ for i in range(len(key)):
 
 
 snow = da.getCollumn(data,13)
-rain = da.getCollumn( data,16)
+rain = da.getCollumn( data,14)
 station = da.getCollumn(data,0)
 date  = da.getCollumn(data,1)
 
-
-
 for i in range ( len ( rain)): # replace the Missing and trace values from the
 
-    if (rain[i] == 'M' or rain[i] == 'T') :
+    if (rain[i] == 'M' ):
         rain [i] = 0.0
-    if (snow[i] == 'M',rain[i] == 'T') :
+
+    if(rain[i] == '  T') :
+        rain[i] = 0.01
+
+    if (snow[i] == 'M'):
         snow[i] = 0.0
+
+    if(snow[i] == '  T') :
+        snow[i] = 0.1
+
     rain[i] = float(rain[i])
     snow[i] = float(snow[i])
 
@@ -48,42 +54,9 @@ avgSnow = 0
 count = 0
 i = 0
 
+# weather complete _________________________________________________________
 
-
-
-#while i < len(snow)- 1: # calculate the average rain and snow for 20 stations
-#    try:
-#        while ( date[i] == date [i+1] ):
-#            avgRain = avgRain + rain[i]
-#            avgSnow = avgSnow + snow[i]
-#
-#
-#            count = count + 1
-#            i = i+1
-#    except IndexError:
-#        print " list index out of range "
-#        #print i
-#    realDate.append(date[i])
-#    i = i + 1
-#    rainDate.append(avgRain/count)
-#    snowDate.append(avgSnow/count)
-#    avgRain = 0
-#    avgSnow = 0
-#    count = 0
-
-
-weatherImformation = [] # create a data structure with date, rain and snow
-
-#weatherImformation = da.addCollumn(weatherImformation,station)
-#weatherImformation = da.addCollumn(weatherImformation,rainDate)
-#weatherImformation = da.addCollumn(weatherImformation,rain)
-#weatherImformation = da.addCollumn(weatherImformation,snow)
-
-# weather complete _________________________________________________________________________
-
-
-
-#_____________________________________________________________________________________________
+#___________________________________________________________________________
 # Now the train file
 
 train =  csv.reader( open( r'train.csv' ) )
@@ -104,15 +77,6 @@ y = 0
 x = []
 o = []
 
-#rubbish Code#while i< len(train): # add rain and snow data to the train file
-#    if ( train[i][0] == weatherImformation[y][0]):
-#        train[i].append(snowDate[y])
-#        train[i].append(rainDate[y])
-#        i = i + 1
-#    else :
-#        y = y + 1
-#
-
 
 
 for i in range(len(train)): # change every value in train to float
@@ -122,6 +86,7 @@ for i in range(len(train)): # change every value in train to float
 print "reached here"
 k = 0
 # create a dictionary for weather data so as to map the station with store
+# adding weather data according to the station and store relation using keys
 stationStore ={}
 for i in range( len(data)):
     stationStore[str(data[i][1])+str(data[i][0])] = [rain[i],snow[i]]
@@ -130,18 +95,6 @@ for i in range( len(train)):
     train[i].append(buff[0])
     train[i].append(buff[1])
 
-#for i in range(len(data)): # adding the weather data according to the station and store relation using keys
-#    while(train[y][0] == data[i][1]):
-#
-#        if(int(keyS[train[y][1]]) == int (data[k][0])) :
-#                train[y].append(rain[k])
-#                train[y].append(snow[k])
-#                y = y + 1
-#                print y
-#        k = k +1
-#    k = 0
-
-print train[0]
 for i in range (len(train)): # change the date to a float value
 
     buff = train[i][0]
@@ -149,41 +102,50 @@ for i in range (len(train)): # change the date to a float value
     train[i][0] = float(buff)
 
 
-#rubbish code #taking in account the wether it was a weather event or not
-#for i in range(len(train)):
 
-#    if (train[i][4] >= 2.0 or train[i][5] >=1.0):
-#        x.append(train[i])
-#y = da.getCollumn(train,3)
-
-c = 0
+c = 3
 for i in range (0,len(train)): # make the x and the output values for testing
-    if ( i != len(train) -1 ) :
-        if (train[i][0] != train[i+1][0]):
+    #if ( i != len(train) -1 ) :
+        #if (train[i][0] != train[i+1][0]):
+                #c = c + 1
+    #if ( c%3 == 0 ):
+    if ( train[i][4] >1 or train[i][5] > 2):
+        if ( i != len(train) -1 ) :
+            if (train[i][0] != train[i+1][0]):
                 c = c + 1
-    if ( c == 3 ):
-        x.append(train[i])
-        o.append(train[i][3])
-        c = 0
-print x[0]
-print train[0]
-da.delCollumn(x,3)
-#train data ready
-#______________________________________________________________________________________________
+        if ( c%3 == 0 ):
+         x.append(train[i])
+         o.append(train[i][3])
 
-from sklearn.svm import SVR
-from sklearn import linear_model
+p = csv.writer(open('preparedData.csv',"wb"))# write data for validating the model
+for i in train:
+    p.writerow(i)
+q = csv.writer(open('testData.csv',"wb"))# write data for validating the model
+for i in x:
+    q.writerow(i)
+
+
+
+da.delCollumn(x,3)
+
+print len(x)
+print len(train)
+#sys.exit('')
+#train data ready
+#___________________________________________________________________________
+
+#from sklearn.svm import SVR
+#from sklearn import linear_model
 from sklearn.tree import DecisionTreeRegressor
 
-#svrRbf = SVR(kernel= 'linear', C = 1e3)
-#svrRbf.fit(x,o)
-clf = DecisionTreeRegressor(max_depth = 2)
+#clf= SVR(kernel= 'rbf', C = 1e3)
+clf = DecisionTreeRegressor(max_depth = 16)
 
 #clf = linear_model.LinearRegression()
 
 clf.fit(x,o)
 print "complete"
-# prepare the test file ______________________________________________________________________
+# prepare the test file ____________________________________________________
 
 for i in range (len(test)): # change date to a float value
 
@@ -192,14 +154,8 @@ for i in range (len(test)): # change date to a float value
     test[i][0] = float(buff)
 
 trainDate = da.getCollumn(train,0)
-#for i in range (  len ( test)):
-#    j = trainDate.index(test[i][0])
-#    test[i].append(train[j][3])
-#    test[i].append(train[j][4])
-#    print i
-
 dateMap = {}
-for i in range(len(train)):
+for i in range(len(train)): # add the extra weather data according to the date
     dateMap[trainDate[i]] = train [i]
 for i in range(len(test)):
     buff = dateMap[test[i][0]]
@@ -207,22 +163,19 @@ for i in range(len(test)):
     test[i].append(buff[4])
 
 
-
-
 test = [[float(i) for i in y ] for y in test]
-print test[0]
-#test file ready______________________________________________________________________
+#test file ready__________________________________________________________
 
 #now predict
-c = csv.writer(open("submit4.csv","wb"))
+c = csv.writer(open("submit5.csv","wb"))
 c.writerow(['id','units'])
-for i in range (len(test)):
+for i in range (len(test)):# add the prediction to the submit file
 
     buff = str(testO[i][1]) + "_" + str(testO[i][2]) + "_" + str(testO[i][0])
 
     c.writerow( [  buff , str(int(max(0,clf.predict(test[i])))) ] )
 
-
+#add the predicting data to a testValidate file so as to test the model locally
 
 
 
