51c8822996615321b1e89931173d5278890af513
abc <abc@abcs-MacBook-Pro.local>
2015-04-29 07:02:06 +0530

    Created a system to test model locally

---
+++ testLocalModel.py
@@ -0,0 +1,46 @@
+import csv
+import math
+import numpy as np
+import six
+import main as da
+
+train= csv.reader(open(r'preparedData.csv'))
+train= [[i for i in y ] for y in train]
+train= [[float(i) for i in y ] for y in train]
+actualValue = da.getCollumn(train,3)
+da.delCollumn(train,3)
+
+x = csv.reader(open(r'testData.csv'))
+x = [[i for i in y ] for y in x]
+x = [[float(i) for i in y ] for y in x]
+o = da.getCollumn(x,3)
+da.delCollumn(x,3)
+print "data uploaded"
+
+from sklearn.tree import DecisionTreeRegressor
+
+clf = DecisionTreeRegressor(max_depth = 14)
+#16:18=45,14=48
+#from sklearn.svm import SVR
+#clf= SVR(kernel= 'rbf', C = 1e3)
+#from sklearn import linear_model
+#clf = linear_model.LinearRegression()
+clf.fit(x,o)
+
+print ' trained'
+y = []
+for i in range(len(train)):
+    y.append(max(0,clf.predict(train[i])))
+print 'predict'
+error = 0
+for i in range (len(y)):
+
+    error = error + (math.log(y[i] + 1)  - math.log(actualValue[i] +1))  ** 2
+
+error = error/ float(len(y))
+error = math.sqrt(error)
+print  error
+
+
+
+
